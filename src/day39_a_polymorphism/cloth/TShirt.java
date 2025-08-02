c500ac4bfb3413e9fc7f5c2eafe6752ce60a1cb0
vrnkxs <kuliba.veronika@gmail.com>
2025-08-02 15:18:23 -0400

    polymorphism, exception intro

---
+++ src/day39_a_polymorphism/cloth/TShirt.java
@@ -0,0 +1,10 @@
+package day39_a_polymorphism.cloth;
+
+// CONCRETE Class
+public class TShirt extends Clothes {
+
+    //@Override
+    public void wearClothes() {
+        System.out.println("Wearing TShirt...");
+    }
+}
